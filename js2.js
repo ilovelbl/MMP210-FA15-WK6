277190aae29a6ac1e917cc1f72643dce295c376f
ilovelbl <hansiyurei@gmail.com>
2015-10-19 15:30:23 -0400

    ..

--- js2.js
+++ js2.js
@@ -3,19 +3,26 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
 
-    drawWindow(50);//call the function
-
-
+    drawWindow(50,50);//call the function
+    drawWindow(50+100+10,50);
+//
+//fill('gray');
+//    rect(50+100+10,50,100,250);
+//
+//    fill(104,250,190);
+//    rect(270,50,100,250);
 }
 
 function draw(){
 
-
 }
 function
-drawWindow(startX,startY,winW,winH){
+drawWindow(startX,startY){
     fill('peru');
-    rect(50,50,100,250);
+    rect(startX,startY,100,250);
+
+
+
 }
 
 

99550939b52be6f17f68db910043f605c429603c
ilovelbl <siyu.han@f1006-23-34167.bmcc.cuny.edu>
2015-10-26 14:13:46 -0400

    widow loops change fix Ver.1

--- js2.js
+++ js2.js
@@ -5,7 +5,7 @@ function setup() {
 
     var x = 0;
     var y = 0;
-    var gridWidth = 900;
+    var gridWidth = windowWidth;
 
     for(var i = 0; i<50; i++){
         console.log(i);
@@ -17,7 +17,7 @@ function setup() {
             //set x back to the begnning
             x = 0;
             //move y down
-            y += 50;
+            y += 300;
         }//end if
     }//end loop
 
@@ -26,12 +26,45 @@ function setup() {
 function draw(){
 
 }
+//function
+//drawWindow(startX,startY){
+//    fill('peru');
+//    stroke('silver');
+//    strokeWeight(5);
+//    rect(startX,startY,100,250);
+//
+//
+//
+//}
 function
 drawWindow(startX,startY){
-    fill('peru');
-    stroke('silver');
-    strokeWeight(5);
-    rect(startX,startY,100,250);
+      strokeWeight(1);
+  stroke('black');
+  fill('peru');
+  rect(startX,startY,125,20);
+
+  //inside black box
+  strokeWeight(1);
+  stroke('black');
+  fill('lightgray');
+  rect(startX+15,startY+20,125-30,150);
+
+  //bottom box
+  strokeWeight(1);
+  stroke('black');
+  fill('peru');
+  rect(startX,startY+170,125,40);
+
+  strokeWeight(4);
+  stroke('peru');
+  line(startX+15,startY+20,startX+15,startY+170);
+  line(startX+63,startY+20,startX+63,startY+170);
+  x = startX + 110;
+  line(x,startY+20,x,startY+170);
+
+  line(startX+15,startY+20,x,startY+20);
+  line(startX+15,startY+20+150/2,x,startY+20+150/2);
+  line(startX+15,startY+20+150,x,startY+20+150);
 
 
 
